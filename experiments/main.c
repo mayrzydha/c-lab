856bac329ad1333b5e85fa3c7713edc9d280beff
mayrzydha <mayrzydha@gmail.com>
2026-09-23 21:46:46 +0700

    docs: update main.c

--- experiments/main.c
+++ experiments/main.c
@@ -1,20 +1,9 @@
-// The first experiment is always about Hello World
-
 #include <stdio.h>
 
 int main()
 {
+    printf("Hello!\n");
+    scanf("Input your name: \n");
 
-    printf("Hello World!");
     return 0;
 }
-
-/*
-What am I doing with this code?
-1. Comment out the #include <stdio.h>
-2. Remove the semicolon after printf
-3. Remove int return type from main
-4. Replace 0 in return with another int value
-5. Replace 0 in return with "a, x, y, z"
-6. Remove the semicolon after return
-*/
